ba153759d899a878d6353eba917e8882d6eab587
Adriel Dias <you@example.com>
2021-05-12 16:52:48 -0300

    Desafio de lógica 01

---
+++ jscript.js
@@ -0,0 +1,13 @@
+let age = 18
+let name = `Adriel`
+let year = 2021
+let sentence1 = `Meu ano de nascinmento é`
+console.log( sentence1, year - age)
+
+let isHuman = true
+let isntHuman = false
+console.log(isHuman)
+
+let number = 5
+let _number = 10
+console.log( number + _number)
